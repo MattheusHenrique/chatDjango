fa9e6cf91f6cdce44c729a0b9db875ae981a9a72
MattheusHenrique <mattheushenrique@gmail.com>
2021-03-06 14:29:23 -0300

    fix bugs

--- chatDjango/settings.py
+++ chatDjango/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'channels',
+    'chat',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -119,3 +121,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# mysite/settings.py
+# Channels
+ASGI_APPLICATION = 'chatDjango.asgi.application'
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
